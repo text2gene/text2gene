7a968408e0333ad2bf4441ac9c329172a6c9f04d
nthmost <naomi@nthmost.com>
2016-02-09 01:02:12 -0800

    get numpy to install from setup.py (YES)

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 import glob, os
 
 from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 setup(
     name = 'pubtatordb',
@@ -12,6 +21,7 @@ setup(
     maintainer_email = 'naomi@nthmost.com',
     license = 'Apache 2.0',
     packages = find_packages(),
+    cmdclass = {'build_ext': build_ext},
     setup_requires = ['numpy'],
     install_requires = [
         'setuptools',
