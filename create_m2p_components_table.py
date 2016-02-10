86d65a3c8d58bc752712db79d182cb4c1b640520
nthmost <naomi@nthmost.com>
2016-02-10 00:52:47 -0800

    use regular expressions to file different variation types into separate tables

--- create_m2p_components_table.py
+++ create_m2p_components_table.py
@@ -2,22 +2,28 @@ from __future__ import print_function
 
 import json
 import math
+import re
 
 from pubtatordb import SQLData
+
+re_DEL = re.compile('^(?P<SeqType>.*?)\|(?P<EditType>DEL)\|(?P<Pos>.*?)\|(?P<Ref>.*?)$')
+re_INS = re.compile('^(?P<SeqType>.*?)\|(?P<EditType>INS)\|(?P<Pos>.*?)\|(?P<Ref>.*?)$')
+re_SUB = re.compile('^(?P<SeqType>.*?)\|(?P<EditType>SUB)\|(?P<Pos>.*?)\|(?P<Ref>.*?)$')
+
+re_dbSNP = re.compile('^rs\d+$')
 
 # Data looks like this:
 """
  {'Components': 'p|SUB|T|415|N', 'Mentions': 'Thr415Asn', 'PMID': 10072423},
- {'Components': '|DEL|202_203|AG',
-  'Mentions': '202-203delAG',
-  'PMID': 10072423},
+ {'Components': '|DEL|202_203|AG','Mentions': '202-203delAG','PMID': 10072423},
  {'Components': '|DEL|255|A', 'Mentions': '255delA', 'PMID': 10072423},
- {'Components': '|INS|321_322|GT',
-  'Mentions': '321-322insGT',
-  'PMID': 10072423},
+ {'Components': '|INS|321_322|GT','Mentions': '321-322insGT','PMID': 10072423},
+ {"PMID": 10371079, "Mentions": "A3243G", "Components": "|SUB|A|3243|G"}, 
+ {"PMID": 10760203, "Mentions": "RS2000", "Components": "rs2000"},
 """
 
 db = SQLData()
+
 
 def create_new_row(row):
     """
@@ -32,11 +38,16 @@ def create_new_row(row):
         components = ['','','','','',]
 
     if len(components) < 5:
+        # it must not be a SUBstitution type. Try to parse out the mutation type.
+        
+
+        # it might be an rs#. We don't handle these right now.
+        
         return 0
 
     new_row = row.copy()
-    new_row['SeqType'] = components[0]
-    new_row['EditType'] = components[1]
+    new_row['SeqType'] = components[0]   # p, c, g, or blank
+    new_row['EditType'] = components[1]  # SUB or DEL or INS
     new_row['Ref'] = components[2]
     new_row['Pos'] = components[3]
     new_row['Alt'] = components[4]
@@ -47,12 +58,12 @@ def create_new_row(row):
 
 
 
-# CREATE TABLE! (Automatically drops if it already exists.)
-#os.popen('mysql -u medgen -pmedgen PubTator < create_m2p_component_table.sql')
-
+# DROP EXISTING TABLES! 
 db.drop_table(TABLENAME)
 
-db.execute('''CREATE TABLE m2p_components (
+# CREATE NEW TABLES!  One each for SUB, DEL, and INS.
+
+db.execute('''CREATE TABLE m2p_SUB (
   PMID int(10) unsigned DEFAULT NULL,
   Components varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   Mentions text COLLATE utf8_unicode_ci NOT NULL,
@@ -62,6 +73,30 @@ db.execute('''CREATE TABLE m2p_components (
   Pos varchar(255) default NULL,
   Alt varchar(255) default NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;''')
+
+db.execute('''CREATE TABLE m2p_DEL (
+  PMID int(10) unsigned DEFAULT NULL,
+  Components varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  Mentions text COLLATE utf8_unicode_ci NOT NULL,
+  SeqType varchar(255) default NULL,
+  EditType varchar(255) default NULL,
+  Ref varchar(255) default NULL,
+  Pos varchar(255) default NULL,
+  Alt varchar(255) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;''')
+
+db.execute('''CREATE TABLE m2p_INS (
+  PMID int(10) unsigned DEFAULT NULL,
+  Components varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  Mentions text COLLATE utf8_unicode_ci NOT NULL,
+  SeqType varchar(255) default NULL,
+  EditType varchar(255) default NULL,
+  Ref varchar(255) default NULL,
+  Pos varchar(255) default NULL,
+  Alt varchar(255) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;''')
+
+
 
 print('Created m2p_components table in PubTator database.  Adding rows...')
 print('')
