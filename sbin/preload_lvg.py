745c5e9ea44a9464822507dd644b172943ce7db5
nthmost <naomi@nthmost.com>
2016-03-11 02:04:11 -0800

    adding preload_lvg script

---
+++ sbin/preload_lvg.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, print_function
+
+import time
+
+from medgen.api import ClinVarDB
+from text2gene import LVG
+
+
+hgvs_examples = ClinVarDB().fetchall('select * from hgvs_examples')
+
+print()
+print('%i HGVS examples found' % len(hgvs_examples))
+print()
+
+def dmesg(hgvs_text, msg):
+    print('[%s] <%i> %s' % (hgvs_text, time.time(), msg))
+
+for entry in hgvs_examples:
+    hgvs_text = entry['hgvs_text']
+    dmesg(hgvs_text, 'collecting')
+    lex = LVG(hgvs_text)
+    dmesg(hgvs_text, '%r' % lex.variants)
+    dmesg(hgvs_text, 'done')
+
