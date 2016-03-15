605def46a8b375097a077ac0ed43222bce2caee1
nthmost <naomi@nthmost.com>
2016-03-15 16:18:54 -0700

    better create_mapping_tables

--- sbin/create_mapping_tables.sql
+++ sbin/create_mapping_tables.sql
@@ -1,21 +1,31 @@
 create table lvg_mappings (
-     hgvs_text varchar(255) primary key not null,
+     hgvs_text varchar(255) not null,
      hgvs_g varchar(255) default NULL,
      hgvs_c varchar(255) default NULL,
      hgvs_n varchar(255) default NULL,
      hgvs_p varchar(255) default NULL
 );
+
+call create_index("lvg_mappings", "hgvs_text,hgvs_g")
+call create_index("lvg_mappings", "hgvs_text,hgvs_c")
+call create_index("lvg_mappings", "hgvs_text,hgvs_n")
+call create_index("lvg_mappings", "hgvs_text,hgvs_p")
 
 create table ncbi_mappings (
-     hgvs_text varchar(255) primary key not null,
+     hgvs_text varchar(255) not null,
      hgvs_g varchar(255) default NULL,
      hgvs_c varchar(255) default NULL,
      hgvs_n varchar(255) default NULL,
      hgvs_p varchar(255) default NULL
 );
 
+call create_index("ncbi_mappings", "hgvs_text,hgvs_g")
+call create_index("ncbi_mappings", "hgvs_text,hgvs_c")
+call create_index("ncbi_mappings", "hgvs_text,hgvs_n")
+call create_index("ncbi_mappings", "hgvs_text,hgvs_p")
+
 create table pubtator_match (
-     hgvs_text varchar(255) primary key not null,
+     hgvs_text varchar(255) not null,
      PMID int(11) default NULL,
      ComponentString varchar(255) default NULL
 );
@@ -23,14 +33,14 @@ create table pubtator_match (
 call create_index("pubtator_match", "hgvs_text,PMID");
 
 create table ncbi_match (
-     hgvs_text varchar(255) primary key not null,
+     hgvs_text varchar(255) not null,
      PMID int(11) default NULL
 );
 
 call create_index("ncbi_match", "hgvs_text,PMID");
 
 create table clinvar_match (
-     hgvs_text varchar(255) primary key not null,
+     hgvs_text varchar(255) not null,
      PMID int(11) default NULL
 );
 
