70f213e67e5e406dcbb71030168edba4c35b5257
nthmost <naomi@nthmost.com>
2016-03-22 13:14:28 -0700

    updated experiment "plain_lvg" to log to file

--- EXPERIMENT_clinvar_examples_plain_lvg.py
+++ EXPERIMENT_clinvar_examples_plain_lvg.py
@@ -1,5 +1,15 @@
+import logging
+
+experiment_name = 'clinvar_examples_plain_lvg'
+
+fh = logging.FileHandler(experiment_name + ".log")
+log = logging.getLogger('text2gene.experiment')
+log.addHandler(fh)
+
+otherlog = logging.getLogger('text2gene')
+otherlog.addHandler(logging.StreamHandler())
 
 from text2gene.training import Experiment
-exper = Experiment('clinvar_examples_plain_lvg', lvg_mode='hgvs')
+exper = Experiment(experiment_name, lvg_mode='lvg')
 exper.run()
 
