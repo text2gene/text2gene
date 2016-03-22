25e58f1da87d0c1528cde31e6fc580a653af4eb4
nthmost <naomi@nthmost.com>
2016-03-22 00:53:53 -0700

    delete everything in lvg_mappings before regenerating

--- sbin/regenerate_lvg_mappings.py
+++ sbin/regenerate_lvg_mappings.py
@@ -2,7 +2,8 @@
 from text2gene.lvg_cached import LVG, HgvsLVGCached
 
 db = HgvsLVGCached()
-all_rows = db.fetchall('select * from hgvslvg_cache')
+db.execute('delete from lvg_mappings where hgvs_text is not NULL')
+all_rows = db.fetchall('select * from hgvslvg_cache order by rand()')
 
 for row in all_rows:
     hgvs_text = row['cache_key'].strip()
