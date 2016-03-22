2fc2438d370052621d1ed229bfb511f528a4b615
nthmost <naomi@nthmost.com>
2016-03-22 00:15:57 -0700

    and again.

--- sbin/regenerate_lvg_mappings.py
+++ sbin/regenerate_lvg_mappings.py
@@ -5,7 +5,7 @@ db = HgvsLVGCached()
 all_rows = db.fetchall('select * from hgvslvg_cache')
 
 for row in all_rows:
-    hgvs_text = row['hgvs_text'].strip()
+    hgvs_text = row['cache_key'].strip()
     lex = LVG(hgvs_text, force_granular=True)
     print(lex)
 
