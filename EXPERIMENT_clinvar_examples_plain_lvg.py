adaf5d4d011269cce74ca81a8dcb9149d6c14e0e
nthmost <naomi@nthmost.com>
2016-03-24 15:24:37 -0700

    up "iteration" in plain_lvg experiment to 1

--- EXPERIMENT_clinvar_examples_plain_lvg.py
+++ EXPERIMENT_clinvar_examples_plain_lvg.py
@@ -1,6 +1,7 @@
 import logging
 
 experiment_name = 'clinvar_examples_plain_lvg'
+iteration = 1
 
 fh = logging.FileHandler(experiment_name + ".log")
 log = logging.getLogger('text2gene.experiment')
@@ -10,6 +11,6 @@ otherlog = logging.getLogger('text2gene')
 otherlog.addHandler(logging.StreamHandler())
 
 from text2gene.training import Experiment
-exper = Experiment(experiment_name, lvg_mode='lvg')
+exper = Experiment(experiment_name, lvg_mode='lvg', iteration=1)
 exper.run()
 
