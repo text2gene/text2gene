24cfd9ac02ca54f6c65b00482229a6232082bbb0
nthmost <naomi@nthmost.com>
2016-03-25 16:46:31 -0700

    stub for GoogleQuery code

---
+++ text2gene/googlequery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import logging
+
+log = logging.getLogger('text2gene.googlequery')
+
+# query example:
+#  GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+# Thor API key:
+#   AIzaSyBbzzCZbm5ccB6MC1e0y_tRFeNBdeoutPo
+
