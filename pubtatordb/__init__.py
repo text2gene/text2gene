5f37ddfa25384ca8f4245186b0078e12b68815ec
nthmost <naomi@nthmost.com>
2016-04-06 16:54:33 -0700

    refactor of pubtator lookups to report more complete information (Mentions & Components with each PMID)

--- pubtatordb/__init__.py
+++ pubtatordb/__init__.py
@@ -1,37 +1,6 @@
 from __future__ import absolute_import
 
-from MySQLdb import ProgrammingError
-
-from .sqldata import SQLData
 from .exceptions import PubtatorDBError
+from .pubtatordb import PubtatorDB
 
-class PubtatorDB(SQLData):
 
-    def _fetchall_or_raise_pubtatordberror(self, sql, comp):
-        try:
-            return self.fetchall(sql)
-        except ProgrammingError as error:
-            # attempt to lookup an edittype that we don't currently handle (e.g. EXT, INV)
-            raise PubtatorDBError('EditType %s currently not handled. (%r)' % (comp.edittype, error))
-
-    def search_FS(self, comp, gene_id, strict=False):
-        if gene_id:
-            sql = "select distinct M.* from gene2pubtator G, m2p_FS M where G.PMID = M.PMID and G.GeneID = {gene_id} and Ref = '{comp.ref}' and Alt = '{comp.alt}' and Pos='{comp.pos}'".format(comp=comp, gene_id=gene_id)
-        else:
-            sql = "select distinct * from m2p_FS where SeqType = '{comp.seqtype}' and Ref = '{comp.ref}' and Alt = '{comp.alt}' and Pos='{comp.pos}'".format(comp=comp)
-        return self.fetchall(sql)
-
-    def search_m2p(self, comp, gene_id, strict=False):
-        # sql = "select distinct M.* from gene2pubtator G, m2p_{comp.edittype} M where G.PMID = M.PMID and G.GeneID = {gene_id} and Pos={comp.pos} and Ref = '{comp.ref}' and Alt = '{comp.alt}' and SeqType='{comp.seqtype}'".format(comp=comp, gene_id=gene_id)
-        if gene_id:
-            sql = "select distinct M.* from gene2pubtator G, m2p_{comp.edittype} M where G.PMID = M.PMID and G.GeneID = {gene_id} and Ref = '{comp.ref}' and Alt = '{comp.alt}' and Pos='{comp.pos}'".format(comp=comp, gene_id=gene_id)
-        else:
-            sql = "select distinct * from m2p_{comp.edittype} where SeqType = '{comp.seqtype}' and Ref = '{comp.ref}' and Alt = '{comp.alt}' and Pos='{comp.pos}'".format(comp=comp)
-        return self._fetchall_or_raise_pubtatordberror(sql, comp)
-
-    def search_proteins(self, comp, gene_id, strict=False):
-        if gene_id:
-            sql = "select distinct M.* from gene2pubtator G, m2p_{comp.edittype} M where G.PMID = M.PMID and G.GeneID = {gene_id} and Pos = '{comp.pos}' and SeqType='p' and Ref = '{comp.ref}'".format(comp=comp, gene_id=gene_id)
-        else:
-            sql = "select distinct * from m2p_{comp.edittype} where SeqType = '{comp.seqtype}' and Pos = '{comp.pos}' and SeqType='p' and Ref = '{comp.ref}'".format(comp=comp)
-        return self._fetchall_or_raise_pubtatordberror(sql, comp)
--- text2gene/pmid_lookups.py
+++ text2gene/pmid_lookups.py
@@ -4,7 +4,7 @@ from medgen.api import GeneID, ClinvarPubmeds
 
 from pubtatordb import PubtatorDB
 from pubtatordb.exceptions import PubtatorDBError
-from hgvs_lexicon import HgvsComponents, RejectedSeqVar
+from hgvs_lexicon import HgvsComponents, RejectedSeqVar, Variant
 
 from .config import log
 
@@ -34,11 +34,17 @@ def clinvar_lex_to_pmid(lex):
     
 
 def pubtator_lex_to_pmid(lex):
-    """ Takes a "lex" object (one of HgvsLVG, NCBIHgvsLVG, or NCBIEnrichedLVG) and uses each variant found in
-    lex.variants to do a search in PubTator for associated PMIDs.  Returns a list of PMIDs.
+    """ Takes an LVG object ("lex") (one of HgvsLVG, NCBIHgvsLVG, or NCBIEnrichedLVG) and uses each
+    variant found in lex.variants to do a search in PubTator for associated PMIDs.
+
+    Returns a dictionary of results mapping HgvsComponents objects to PMIDs found -- i.e.:
+
+        { hgvs_text: {'comp': HgvsComponents object,
+                      'pmids': [<pmids>]
+                     }
 
     :param lex: lexical variant object (see above options)
-    :return: list of pmids found in Clinvar
+    :return: dictionary of results
     """
     try:
         gene_id = GeneID(lex.gene_name)
@@ -57,7 +63,7 @@ def pubtator_lex_to_pmid(lex):
                 log.debug('[%s] Rejected sequence variant: %r' % (lex.seqvar, seqvar))
                 continue
 
-            log.info('[%s] (%s) %s %s', lex.seqvar, seqvar, seqtype, components)
+            log.info('[%s] [[%s]] %s', lex.seqvar, seqvar, components)
             try:
                 if seqtype == 'p':
                     results = pubtator_db.search_proteins(components, gene_id)
@@ -69,3 +75,71 @@ def pubtator_lex_to_pmid(lex):
                 log.info('[%s] (%s) %r', lex.seqvar, seqvar, error)
 
     return list(pmids)
+
+
+def pubtator_results_for_seqvar(seqvar_or_hgvs_text, gene_id):
+    """ Takes a SequenceVariant or hgvs_text string.
+    Returns a dictionary of results mapping hgvs_text to a list of results from pubtator, i.e.:
+
+        { hgvs_text: [ <dictionaries representing matching results from pubtator> ] }
+
+    :param seqvar_or_hgvs_text: hgvs_text or SequenceVariant object
+    :oaram gene_id: id of gene associated with variant (required)
+    :return: dictionary of results
+    :raises: RejectedSeqVar, PubtatorDBError
+    """
+    seqvar = Variant(seqvar_or_hgvs_text)
+    hgvs_text = '%s' % seqvar
+
+    result = {hgvs_text: []}
+
+    components = HgvsComponents(seqvar)
+
+    if seqvar.type == 'p':
+        result[hgvs_text] = pubtator_db.search_proteins(components, gene_id)
+    else:
+        result[hgvs_text] = pubtator_db.search_m2p(components, gene_id)
+
+    return result
+
+
+def pubtator_results_for_lex(lex):
+    """ Takes an LVG object ("lex") (one of HgvsLVG, NCBIHgvsLVG, or NCBIEnrichedLVG) and uses each
+    variant found in lex.variants to do a search in PubTator for associated PMIDs.
+
+    Returns a dictionary of results mapping hgvs_text to PMIDs found -- i.e.:
+
+        { hgvs_text: {'components': HgvsComponents object,
+                      'pmids': [<pmids>]
+                     }
+        }
+
+    :param lex: lexical variant object (see above options)
+    :return: dictionary of results
+    """
+    try:
+        gene_id = GeneID(lex.gene_name)
+    except TypeError:
+        # no gene_name? it happens -- but our results will be basically bunk without it.
+        return {}
+
+    log.info('[%s] %s (Gene ID: %s)', lex.seqvar, lex.gene_name, gene_id)
+
+    results = {}
+    for seqtype in lex.variants:
+        for seqvar in lex.variants[seqtype].values():
+
+            try:
+                result = pubtator_results_for_seqvar(seqvar, gene_id)
+                results.update(result)
+                for row in results['%s' % seqvar]:
+                    log.info('[%s] [[%s]] Mentions: %s  PMID: %s  Components: %s', lex.seqvar, seqvar,
+                             row['Mentions'], row['PMID'], row['Components'])
+
+            except RejectedSeqVar:
+                log.debug('[%s] [[%s]] HgvsComponents raised RejectedSeqVar', lex.seqvar, seqvar)
+
+            except PubtatorDBError as error:
+                log.info('[%s] [[%s]] %r', lex.seqvar, seqvar, error)
+
+    return results
