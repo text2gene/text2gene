143240ecaaca83a9d8336642c1bf8008fe67c19b
nthmost <naomi@nthmost.com>
2016-04-14 09:07:40 +0000

    fixup to preload script

--- sbin/preload_ncbi_enriched_lvg.py
+++ sbin/preload_ncbi_enriched_lvg.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import, print_function
 import time
 
 from medgen.api import ClinVarDB
-from text2gene import NCBIEnrichedLVG, NCBIHgvs2Pmid
+from text2gene.api import LVGEnriched, NCBIHgvs2Pmid
+from hgvs_lexicon.exceptions import CriticalHgvsError
 
 
-hgvs_examples = ClinVarDB().fetchall('select * from hgvs_examples')  # limit 10')
+hgvs_examples = ClinVarDB().fetchall('select * from clinvar_hgvs order by rand() limit 200000')  # limit 10')
 
 print()
 print('%i HGVS examples found' % len(hgvs_examples))
@@ -18,7 +19,12 @@ def dmesg(hgvs_text, msg):
 for entry in hgvs_examples:
     hgvs_text = entry['hgvs_text']
     dmesg(hgvs_text, 'collecting')
-    lex = NCBIEnrichedLVG(hgvs_text)
+    try:
+        lex = LVGEnriched(hgvs_text)
+    except CriticalHgvsError as error:
+        print(error)
+        continue
+
     pmids = NCBIHgvs2Pmid(hgvs_text)
     dmesg(hgvs_text, 'PMIDs: %r' % pmids)
     dmesg(hgvs_text, '%r' % lex.variants)
