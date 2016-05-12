4b3c91e7455acd79a23af8e9f3e71d0d8641fa6f
nthmost <naomi@nthmost.com>
2016-05-12 02:06:45 -0700

    added simple script to run google query results on a list of hgvs strings in a file

---
+++ google_query_result_test.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+import sys
+
+from text2gene.googlequery import GoogleQuery
+from text2gene.api import LVGEnriched
+from text2gene.exceptions import Text2GeneError
+
+def dmesg(hgvs_text, msg):
+    print('[%s] %s' % (hgvs_text, msg))
+
+def print_result(hgvs_text, cseres, idx):
+    if cseres.pmid:
+        dmesg(hgvs_text, '%i) Resolved: %s' % (idx, cseres.pmid))
+    else:
+        dmesg(hgvs_text, '%i) Unresolved:' % idx)
+    dmesg(hgvs_text, '    url: %s' % cseres.url)
+    dmesg(hgvs_text, '    doi: %s' % cseres.doi)
+
+try:
+    hgvs_file = sys.argv[1]
+except IndexError:
+    print('Supply filename of text file containing list of hgvs_text samples as argument to this script.')
+    sys.exit()
+
+with open(hgvs_file) as fileh:
+    hgvs_samples = [item for item in fileh.read().split('\n') if item.strip() != '']
+
+for hgvs_text in hgvs_samples:
+    lex = LVGEnriched(hgvs_text)
+    
+    try:
+        cse_results = GoogleQuery(lex)
+    except Text2GeneError as error:
+        print('@@@ Error querying Google')
+        dmesg(hgvs_text, '%r' % error)
+        print()
+        dmesg(hgvs_text, 'Google query: %s' % gcse.build_query())
+        continue
+
+    dmesg(hgvs_text, 'Google CSE Results: %i' % len(cse_results))
+    resolved = []
+    idx = 0
+    for cseres in cse_results:
+        if cseres.pmid:
+            resolved.append(cseres)
+        print_result(hgvs_text, cseres, idx)
+        idx += 1
+
+    dmesg(hgvs_text, 'TOTAL RESOLVED: %i / %i' % (len(resolved), len(cse_results)))
+    print()
+
+
