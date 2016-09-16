99753c5f8f5f0704db4c8941f42c3bc329e33b43
nthmost <naomi@nthmost.com>
2016-09-16 14:09:22 -0700

    added baseline clinvar protein search to pubtatordb (i know, needs refactoring.)

---
+++ pubtatordb/clinvardb.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import, unicode_literals
+
+from MySQLdb import ProgrammingError
+
+from .sqldata import SQLData
+
+class ClinVarAminoDB(SQLData):
+
+    def _fetchall_or_raise_exception(self, sql, comp, *args):
+        try:
+            return self.fetchall(sql, *args)
+        except ProgrammingError as error:
+            # attempt to lookup an edittype that we don't currently handle (e.g. EXT, INV)
+            raise Exception('EditType %s currently not handled. (%r)' % (comp.edittype, error))
+
+    def search_clinvar_strict(self, comp, gene_id):
+        if gene_id:
+            sql = 'select distinct * from clinvar.variant_components where GeneID=%s and Ref=%s and Alt=%s and Pos=%s'
+            args = (gene_id, comp.ref, comp.alt, comp.pos)
+        else:
+            sql = 'select distinct * from clinvar.variant_components where Ref=%s and Alt=%s and Pos=%s'
+            args = (comp.ref, comp.alt, comp.pos)
+        return self.fetchall(sql, *args)
+
+    def search_clinvar_loose(self, comp, gene_id):
+        if gene_id:
+            sql = 'select distinct * from clinvar.variant_components where GeneID=%s and Ref=%s and Pos=%s'
+            args = (gene_id, comp.ref, comp.pos)
+        else:
+            sql = 'select distinct * from clinvar.variant_components where Ref=%s and Pos=%s'
+            args = (comp.ref, comp.pos)
+        return self.fetchall(sql, *args)
+
+    def search(self, comp, gene_id, strict=False):
+        if strict:
+            return self.search_clinvar_strict(comp, gene_id)
+        else:
+            return self.search_clinvar_loose(comp, gene_id)
+
+        return self._fetchall_or_raise_exception(sql, comp, *args)
+
