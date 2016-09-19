cc65b8037b988e0b453aeeac3d364c7ece1869f7
nthmost <naomi@nthmost.com>
2016-09-19 16:47:22 -0700

    use regular expressions to qualify hgvs inputs first.

--- sbin/create_clinvar_components_table.py
+++ sbin/create_clinvar_components_table.py
@@ -9,9 +9,18 @@ from text2gene.api import LVGEnriched
 from pubtatordb.sqldata import SQLData
 from pubtatordb.config import get_process_log
 
+import re
+
+# HGVS regex: https://regex101.com/r/yI1kR6/1
+re_hgvs = re.compile('(?P<prefix>N[A-Z])_(?P<transcript>\d+\.\d+)(\((?P<symbol>\w+)\))?\:(?P<edit>.*?)\s')
+
+# LRG regex: https://regex101.com/r/bT1gW3/2
+#re_lrg = re.compile('(?P<prefix>LRG)_(?P<position>\w+)\:(?<edit>[a-z]\.\w+)')
+
 log = get_process_log('logs/clinvar_components_table.log')
 
 
+ALREADY_DONE = 0
 ERRORS = 0
 GOOD = 0
 
@@ -40,19 +49,25 @@ def process_row(dbrow):
         return comp
 
     for option in ['variant_name', 'HGVS_c']:
-        hgvs_text = dbrow[option]
-        if hgvs_text is None:
+        entry = dbrow[option]
+        if entry is None:
             continue
 
         # basic string cleaning
-        hgvs_text = hgvs_text.strip()
-
-        # look out for variants in this format:  NM_001363.4(DKC1):c.1058C>T (p.Ala353Val)
-        if (hgvs_text.split()) > 1:
-            hgvs_text = hgvs_text.split()[0]
+        entry = entry.strip()
 
         # beware the pesky N-dash in at least 1 observed ClinVar db entry.
-        hgvs_text = hgvs_text.replace('\u2013', '-')
+        entry = entry.replace('\u2013', '-')
+
+        hgvs_text = ''
+        match = re_hgvs.match(entry)
+        if match:
+            stuff = match.groupdict()
+            hgvs_text = stuff['prefix'] + '_' + stuff['transcript'] + ':' + stuff['edit']
+
+        # TODO: LRG 
+        else:
+            continue
 
         try:
             seqvar = Variant(hgvs_text)
@@ -76,15 +91,23 @@ def process_row(dbrow):
     return None
 
 
+def print_dbrow_with_components(dbrow, qualifier):
+    print("""[{variant_name}] (VariationID: {VariationID}) {qual}
+    \tRef:{Ref}
+    \tPos:{Pos}
+    \tAlt:{Alt}
+    """.format(qual=qualifier.upper(), **dbrow))
+
+
 def add_components_to_row(db, dbrow, comp):
     print('ADD:')
     try:
-        print('\tRef:%s' % comp.ref)
-        print('\tPos:%s' % comp.pos)
-        print('\tAlt:%s' % comp.alt)
-
         db.execute('update variant_components set Ref=%s, Pos=%s, Alt=%s where variant_name=%s',
                         dbrow['Ref'], dbrow['Pos'], dbrow['Alt'], dbrow['variant_name'])
+        dbrow['Ref'] = comp.ref
+        dbrow['Alt'] = comp.alt
+        dbrow['Pos'] = comp.pos
+        print_dbrow_with_components(dbrow, 'added')
 
     except AttributeError as error:
         print(error)
@@ -95,6 +118,7 @@ def add_components_to_row(db, dbrow, comp):
 def main():
     global ERRORS
     global GOOD
+    global ALREADY_DONE
 
     db = ClinVarDB()
     log.info('Started "create_clinvar_components_table.py"')
@@ -113,26 +137,42 @@ def main():
 
     rows = db.fetchall('select * from variant_components order by rand()')
 
-    worked = 0
-    errors = 0
     count = 0
     for row in rows:
         count += 1
         print(count, '/', res['cnt'], ':', row['variant_name'])
+
+        if row['Ref'] and row['Pos']:
+            ALREADY_DONE += 1
+            GOOD += 1
+            print_dbrow_with_components(row, 'existing')
+            continue
+
         components = process_row(row)
         if components:
             GOOD += 1
-            print('[%s] added components: %r' % (row['variant_name'], components))
+            #print('[%s] added components: %r' % (row['variant_name'], components))
             add_components_to_row(db, row, components)
             log.info(row['variant_name'] + ' - no usable amino acid components')
         else:
             ERRORS += 1
             print('[%s] no usable amino acid components' % row['variant_name'])
             log.info(row['variant_name'] + ' - no usable amino acid components')
+
+    return dbtotal
     
 if __name__=='__main__':
-    main()
+    dbtotal = main()
 
+    print('@@@ FINISHED!!!!')
+    print()
     print('unusable:', ERRORS)
     print('good:', GOOD)
+    print('already done:', ALREADY_DONE)
+    print('newly done:', (GOOD - ALREADY_DONE))
+    print('-----------')
+    print('total:', (ERRORS + GOOD)) 
+    print('total rows in DB:', dbtotal)
+    print()
+
 
