e11226409a5cd4ca4c9bd2fb900136e02799ebd0
nthmost <naomi@nthmost.com>
2019-10-04 07:23:19 +0000

    removed all NCBI related tests

--- tests/test_cached.py
+++ tests/test_cached.py
@@ -2,14 +2,12 @@
 import unittest
 
 from text2gene.lvg_cached import VariantLVGCached
-from text2gene.ncbi import NCBIEnrichedLVGCachedQuery
-from text2gene import LVG, LVGEnriched
+from text2gene import LVG
 
 test_hgvs_c = 'NM_001232.3:c.919G>C'
 test_hgvs_n = 'NM_194248.2:n.285C>T'
 
 lvg_cache_db = VariantLVGCached()
-ncbi_lvg_cache_db = NCBIEnrichedLVGCachedQuery()
 
 class TestVariantLVGCached(unittest.TestCase):
 
@@ -26,17 +24,4 @@ class TestVariantLVGCached(unittest.TestCase):
 
         lex = LVG(test_hgvs_c)
         assert lex.hgvs_text == test_hgvs_c
-
-    def test_ncbi_enriched_lvg_cache(self):
-        ncbi_lvg_cache_db.delete(test_hgvs_n)
-        lex = LVGEnriched(test_hgvs_n)
-        result = ncbi_lvg_cache_db.retrieve(test_hgvs_c)
-        #TODO: fix
-        #assert result is not None
-
-        lex = LVGEnriched(test_hgvs_n)
-        assert lex.hgvs_text == test_hgvs_n
-        
-    #def test_LVG_granular(self):
-    #    lex = LVG(test_hgvs_c)
 
