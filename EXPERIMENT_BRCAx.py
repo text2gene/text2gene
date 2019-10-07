815b81d379d169bfa45c86c1a9b1a2df637410c3
nthmost <naomi@nthmost.com>
2019-10-07 21:44:29 +0000

    added BRCAx (1,2) experiment

--- EXPERIMENT_BRCAx.py
+++ EXPERIMENT_BRCAx.py
@@ -3,9 +3,6 @@ import logging
 from metavariant import Variant
 from text2gene.experiment import Experiment
 
-from medgen import ClinvarDB
-
-
 experiment_name = 'BRCAx'
 iteration = 1
 
@@ -17,7 +14,7 @@ otherlog = logging.getLogger('text2gene')
 otherlog.addHandler(logging.StreamHandler())
 otherlog.setLevel(logging.DEBUG)
 
-LOADED_EXAMPLES = open('data/samples_BRCAx').readlines()
+LOADED_EXAMPLES = open('data/samples_BRCAx.tsv').readlines()
 
 exper = Experiment(experiment_name, lvg_mode='lvg', iteration=iteration,
                     hgvs_examples=LOADED_EXAMPLES,)
